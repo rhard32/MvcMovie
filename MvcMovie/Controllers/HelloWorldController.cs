5948a7f292d37d7efc3cb79f862130ff36b998f5
rhard32 <rhard32@gmail.com>
2015-03-30 11:45:19 -0400

    Code Added to controller

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: HelloWorld
-        public ActionResult Index()
+        // 
+        // GET: /HelloWorld/ 
+
+        public string Index()
         {
-            return View();
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
         }
     }
 }
