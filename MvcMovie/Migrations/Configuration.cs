7fde498cdc43747887da3baf4e9b0330d1f7ebd7
rhard32 <rhard32@gmail.com>
2015-04-03 11:09:46 -0400

    Ratings Migration

--- MvcMovie/Migrations/Configuration.cs
+++ MvcMovie/Migrations/Configuration.cs
@@ -22,6 +22,7 @@ using System.Linq;
             Title = "When Harry Met Sally",
             ReleaseDate = DateTime.Parse("1989-1-11"),
             Genre = "Romantic Comedy",
+            Rating = "PG",
             Price = 7.99M
         },
 
@@ -30,6 +31,7 @@ using System.Linq;
              Title = "Ghostbusters ",
              ReleaseDate = DateTime.Parse("1984-3-13"),
              Genre = "Comedy",
+             Rating = "PG",
              Price = 8.99M
          },
 
@@ -38,6 +40,7 @@ using System.Linq;
              Title = "Ghostbusters 2",
              ReleaseDate = DateTime.Parse("1986-2-23"),
              Genre = "Comedy",
+             Rating = "PG",
              Price = 9.99M
          },
 
@@ -46,6 +49,7 @@ using System.Linq;
            Title = "Rio Bravo",
            ReleaseDate = DateTime.Parse("1959-4-15"),
            Genre = "Western",
+           Rating = "G",
            Price = 3.99M
        }
    );
